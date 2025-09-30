8912f14c4d477ea692a527547e6429771942fe94
ShakilJoy31 <joy31a@gmail.com>
2025-09-30 12:19:29 +0600

    Data fetching url is changed

--- src/utils/constant/appConfiguration.ts
+++ src/utils/constant/appConfiguration.ts
@@ -32,5 +32,5 @@ export const appConfiguration: IConfigurationProps = {
 // http://localhost:3000
 
 // Production Url........
-// https://gramer-haat-3zpk.vercel.app
-export const baseUrl = 'https://gramer-haat-3zpk.vercel.app';
+// https://gramer-haat.vercel.app
+export const baseUrl = 'https://gramer-haat.vercel.app';
